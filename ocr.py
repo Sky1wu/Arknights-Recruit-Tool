b25a6d3d0fba722c2242c908e747ddb885b6e97e
Sky1Wu <wtx919463944@gmail.com>
2024-08-11 15:30:58 +0800

    新增干员

--- ocr.py
+++ ocr.py
@@ -2,7 +2,7 @@ import pytesseract
 
 
 def ocr(img):
-    whitelist = '高级远程位输出狙击干员近战支援卫治疗医防护重装爆发生存群攻削弱术师费用回复先锋特种资深机械移减速新手控场快活辅助召唤'
+    whitelist = '高级远程位输出狙击干员近战支援卫治疗医防护重装爆发生存群攻削弱术师费用回复先锋特种资深机械移减速新手控场快活辅助召唤元素'
 
     text = pytesseract.image_to_string(
         img, lang='ark_recruit', config='--dpi 300 --oem 0 --psm 8 -c tessedit_char_whitelist='+whitelist)
